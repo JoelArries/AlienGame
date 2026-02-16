b67280ba226d92685dc21ba7b41fee334f777fe0
Joel Arries <joa00266@students.stir.ac.uk>
2026-02-16 11:50:34 +0000

    implemented key methods. alien can be drawn to the screen in the animation loop. predator walking sprite sheet added to images

--- src/AlienGame.java
+++ src/AlienGame.java
@@ -29,6 +29,7 @@ public class AlienGame extends GameCore{
     public static void main(String[] args){
         AlienGame alienGame = new AlienGame();
         alienGame.init();
+        alienGame.run(false, screenWidth, screenHeight);
     }
 
     public void init(){
@@ -41,18 +42,59 @@ public class AlienGame extends GameCore{
         alienWalking.loadAnimationFromSheet("images/spritesheet6.png", 6, 1, 100);
 
         alien = new Sprite(alienWalking);
+
+        initialiseGame();
+        System.out.println(tmap);
     }
 
     public void initialiseGame(){
-
+        timeElapsed = 0;
+        alien.setScale(0.15f);
+        alien.setPosition(200, 200);
+        alien.setVelocity(0, 0);
+        alien.show();
     }
 
     public void draw(Graphics2D g){
+        //make sure to draw "back to front": BackG->ForeG->sprite
+        int xOffset = -(int)alien.getX() + 200;
+        int yOffset = -(int)alien.getY() + 200;
 
+        g.setBackground(Color.white);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        //offsets are applied to clouds here and then drawn
+
+        //offsets applied to tilemap and drawn
+        tmap.draw(g, xOffset, yOffset);
+
+        alien.setOffsets(xOffset, yOffset);
+        alien.drawTransformed(g);
+
+        //show score implemented here if wanted
+
+        //debug mode here when wanted
     }
 
-    public void update(long timeElapsed){
+    //drawCollidedTiles()
 
+    public void update(long timeElapsed){
+        alien.setVelocityY(alien.getVelocityY()+(gravity*timeElapsed));
+        alien.setAnimationSpeed(1.0f);
+
+        alien.update(timeElapsed);
+    }
+
+    public void handleScreenEdge(Sprite s, TileMap tmap, long timeElapsed){
+        float difference = s.getY() + s.getHeight() - tmap.getPixelHeight();
+        if (difference > 0)
+        {
+        	// Put the player back on the map according to how far over they were
+        	s.setY(tmap.getPixelHeight() - s.getHeight() - (int)(difference)); 
+        	
+        	// and make them bounce
+        	s.setVelocityY(-s.getVelocityY()*0.75f);
+        }
     }
 
     public void keyPressed(KeyEvent e){
