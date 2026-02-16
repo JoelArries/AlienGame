84bbbf8f8f3ee0d21cf1c1418a7e0fe52f57b04d
Joel Arries <joa00266@students.stir.ac.uk>
2026-02-16 14:51:18 +0000

    implemented key pressed and released methods. added astronaut spritesheets and animation

--- src/AlienGame.java
+++ src/AlienGame.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import game2D.*;
 
 //Student ID: 3329293
+/*
+Tile height: 32. Tile width: 32.
+alien height: 552. alien width: 251
+ */
 
 public class AlienGame extends GameCore{
 
@@ -15,9 +19,20 @@ public class AlienGame extends GameCore{
     float gravity = 0.0001f;
     float walkSpeed = 0.05f;
 
+    //Game State Flags
+    boolean moveRight = false;
+    boolean moveLeft = false;
+    boolean idle = true;
+    //Not sure about this one 
+    boolean turning = false;
+
     //Game Resources
-    Animation alienWalking;
+    ArrayList<Sprite> sprites = new ArrayList<>();
+
     Sprite alien;
+    Sprite astronaut;
+
+    Animation alienWalking, astronautWalking;
 
     TileMap tmap = new TileMap();
 
@@ -40,8 +55,11 @@ public class AlienGame extends GameCore{
 
         alienWalking = new Animation();
         alienWalking.loadAnimationFromSheet("images/spritesheet6.png", 6, 1, 100);
+        astronautWalking = new Animation();
+        astronautWalking.loadAnimationSeries("images/walk.png", 3, 4, 100, 6, 5);
 
         alien = new Sprite(alienWalking);
+        astronaut = new Sprite(astronautWalking);
 
         initialiseGame();
         System.out.println(tmap);
@@ -49,10 +67,18 @@ public class AlienGame extends GameCore{
 
     public void initialiseGame(){
         timeElapsed = 0;
-        alien.setScale(0.15f);
+        //scale the alien to be the width and height of a tile.
+        alien.setScale(0.13f,0.058f);
         alien.setPosition(200, 200);
         alien.setVelocity(0, 0);
         alien.show();
+
+        astronaut.setScale(0.92f, 0.7f);
+        astronaut.setPosition(100, 100);
+        astronaut.setVelocity(0, 0);
+        astronaut.show();
+        sprites.add(alien);
+        sprites.add(astronaut);
     }
 
     public void draw(Graphics2D g){
@@ -70,22 +96,48 @@ public class AlienGame extends GameCore{
 
         alien.setOffsets(xOffset, yOffset);
         alien.drawTransformed(g);
+        astronaut.setOffsets(xOffset, yOffset);
+        astronaut.drawTransformed(g);
+
 
         //show score implemented here if wanted
 
         //debug mode here when wanted
     }
 
-    //drawCollidedTiles()
 
     public void update(long timeElapsed){
+        /*
+        * TODO: 
+        * iterate through sprite arraylist updating velocity
+        * and animation speed for each sprite it concerns
+         */
+
         alien.setVelocityY(alien.getVelocityY()+(gravity*timeElapsed));
         alien.setAnimationSpeed(1.0f);
+        astronaut.setVelocityY(astronaut.getVelocityY() + (gravity*timeElapsed));
+        astronaut.setAnimationSpeed(1.0f);
 
-        alien.update(timeElapsed);
+        if(moveRight){
+            astronaut.setVelocityX(walkSpeed);
+        }
+        else{
+            astronaut.setVelocityX(0);
+        }
+        
+        for(Sprite s : sprites){
+            s.update(timeElapsed);
+            //handleScreenEdge(s, tmap, timeElapsed);
+        }
+        
     }
 
     public void handleScreenEdge(Sprite s, TileMap tmap, long timeElapsed){
+        /*
+        * TODO:
+        * implement this with tile collision instead after initial rough tests
+        */
+
         float difference = s.getY() + s.getHeight() - tmap.getPixelHeight();
         if (difference > 0)
         {
@@ -93,12 +145,17 @@ public class AlienGame extends GameCore{
         	s.setY(tmap.getPixelHeight() - s.getHeight() - (int)(difference)); 
         	
         	// and make them bounce
-        	s.setVelocityY(-s.getVelocityY()*0.75f);
+        	s.setVelocityY(-s.getVelocityY()*0.5f);
         }
     }
 
     public void keyPressed(KeyEvent e){
+        int keyPressed = e.getKeyCode();
 
+        switch(keyPressed)
+        {
+            case KeyEvent.VK_RIGHT  : moveRight = true; break;
+        }
     }
 
     public boolean boundingBoxCollision(Sprite s1, Sprite s2){return false;}
@@ -108,6 +165,11 @@ public class AlienGame extends GameCore{
     }
 
     public void keyReleased(KeyEvent e){
+        int keyReleased = e.getKeyCode();
 
+        switch (keyReleased) {
+            case KeyEvent.VK_RIGHT  : moveRight = false; break;
+                
+        }
     }
 }
